8d1f678551fb7c9c49f044a0880aa30b080127a7
Andrea Lepori <al@0hm.ch>
2024-05-07 13:55:52 +0900

    lzo remove some casting operations

--- lzo-compression/simpleLzo.c
+++ lzo-compression/simpleLzo.c
@@ -89,7 +89,7 @@ int main(int argc, char** argv)
 				}
 				ll_end = ip + ll;
 
-				memset(dictMem, 0, ((unsigned int)1 << 13 /*DBITS*/) * sizeof(unsigned short));
+				memset(dictMem, 0, (1 << 13) * sizeof(unsigned short));
 
 				unsigned char* ip_block;
 				unsigned char* in_end = pInputBuffer + ll;
@@ -103,7 +103,7 @@ int main(int argc, char** argv)
 				ip_block = pInputBuffer;
 				ii = pInputBuffer;
 
-				ip_block += 5;
+				ip_block += 4;
 				unsigned char cond_ipblock;
 				cond_ipblock = ip_block < ip_block_end;
 				while (cond_ipblock) {
@@ -116,22 +116,42 @@ int main(int argc, char** argv)
 					unsigned int dv;
 					unsigned int dindex;
 
-					dv = ((unsigned int*) ip_block)[0];
+					dv = ip_block[0];
+					dv |= ip_block[1] << 8;
+					dv |= ip_block[2] << 16;
+					dv |= ip_block[3] << 24;
+
 					dindex = ((dv * 0x1824429D) >> 19) & 0x1FFF;	/* Determine dictionary index that maps to the new data value.		*/
 					m_pos = pInputBuffer + dict[dindex];	/* Obtain absolute address of the current dictionary entry match.	*/
 					dict[dindex] = ip_block-pInputBuffer;		/* Update dictionary entry to point to the latest value, store relative offset. */
 
-					while (dv != ((unsigned int*) m_pos)[0]) {
+					unsigned int while_cond1 = 0;
+					while_cond1 = m_pos[0];
+					while_cond1 |= m_pos[1] << 8;
+					while_cond1 |= m_pos[2] << 16;
+					while_cond1 |= m_pos[3] << 24;
+
+					while (dv != while_cond1) {
 						unsigned int diff;
 						diff = ip_block - ii;
 						ip_block += 1 + (diff >> 5);
 						if (ip_block >= ip_block_end) {
 							break;
 						}
-						dv = ((unsigned int*) ip_block)[0];
+
+						dv = ip_block[0];
+						dv |= ip_block[1] << 8;
+						dv |= ip_block[2] << 16;
+						dv |= ip_block[3] << 24;
+
 						dindex = ((dv * 0x1824429D) >> 19) & 0x1FFF;	/* Determine dictionary index that maps to the new data value.		*/
 						m_pos = pInputBuffer + dict[dindex];	/* Obtain absolute address of the current dictionary entry match.	*/
 						dict[dindex] = ip_block-pInputBuffer;		/* Update dictionary entry to point to the latest value, store relative offset. */
+
+						while_cond1 = m_pos[0];
+						while_cond1 |= m_pos[1] << 8;
+						while_cond1 |= m_pos[2] << 16;
+						while_cond1 |= m_pos[3] << 24;
 					}
 
 					/* a match */
@@ -142,25 +162,18 @@ int main(int argc, char** argv)
 						if (t_inner <= 3)
 						{
 							op -= 2;
-							op[0] |= (unsigned char)(t_inner);
+							op[0] |= t_inner;
 							op += 2;
-							((unsigned int*) op)[0] = ((unsigned int*) ii)[0];
-							op += t_inner;
-						}
-						else if (t_inner <= 16)
-						{
-							op[0] = (unsigned char)(t_inner - 3);
-							op++;
-							((unsigned int*) op)[0] = ((unsigned int*) ii)[0];
-							((unsigned int*) (op))[1] = ((unsigned int*) (ii))[1];
-							((unsigned int*) (op))[2] = ((unsigned int*) (ii))[2];
-							((unsigned int*) (op))[3] = ((unsigned int*) (ii))[3];
+							op[0] = ii[0];
+							op[1] = ii[1];
+							op[2] = ii[2];
+							op[3] = ii[3];
 							op += t_inner;
 						}
 						else
 						{
 							if (t_inner <= 18) {
-								op[0] = (unsigned char)(t_inner - 3);
+								op[0] = (t_inner - 3);
 								op++;
 							} else
 							{
@@ -168,10 +181,10 @@ int main(int argc, char** argv)
 								op++;
 								unsigned int tt = t_inner - 18;
 								for (tt = t_inner-18; tt > 255; tt-=255) {
-									((unsigned char *) op)[0] = 0;
+									op[0] = 0;
 									op++;
 								}
-								op[0] = (unsigned char)(tt);
+								op[0] = tt;
 								op++;
 							}
 
@@ -183,7 +196,6 @@ int main(int argc, char** argv)
 						}
 					}
 					
-
 
 					m_len = 4;
 					unsigned int bytematch;
@@ -226,16 +238,16 @@ int main(int argc, char** argv)
 					if (m_len <= 8 && m_off <= 0x800)
 					{
 						m_off -= 1;
-						op[0] = (unsigned char)(((m_len - 1) << 5) | ((m_off & 7) << 2));
+						op[0] = (((m_len - 1) << 5) | ((m_off & 7) << 2));
 						op++;
-						op[0] = (unsigned char)(m_off >> 3);
+						op[0] = (m_off >> 3);
 						op++;
 					}
 					else if (m_off <= 0x4000)
 					{
 						m_off -= 1;
 						if (m_len <= 33) {
-							op[0] = (unsigned char)(0x20 | (m_len - 2));
+							op[0] = (0x20 | (m_len - 2));
 							op++;
 						} else
 						{
@@ -245,40 +257,40 @@ int main(int argc, char** argv)
 							while (m_len > 255)
 							{
 								m_len -= 255;
-								((unsigned char *) op)[0] = 0;
+								op[0] = 0;
 								op++;
 							}
-							op[0] = (unsigned char)(m_len);
+							op[0] = (m_len);
 							op++;
 						}
-						op[0] = (unsigned char)(m_off << 2);
+						op[0] = (m_off << 2);
 						op++;
-						op[0] = (unsigned char)(m_off >> 6);
+						op[0] = (m_off >> 6);
 						op++;
 					}
 					else
 					{
 						m_off -= 0x4000;
 						if (m_len <= 0x9) {
-							op[0] = (unsigned char)(0x10 | ((m_off >> 11) & 8) | (m_len - 2));
+							op[0] = (0x10 | ((m_off >> 11) & 8) | (m_len - 2));
 							op++;
 						} else
 						{
 							m_len -= 0x9;
-							op[0] = (unsigned char)(0x10 | ((m_off >> 11) & 8));
+							op[0] = (0x10 | ((m_off >> 11) & 8));
 							op++;
 							while (m_len > 255)
 							{
 								m_len -= 255;
-								((unsigned char *) op)[0] = 0;
+								op[0] = 0;
 								op++;
 							}
-							op[0] = (unsigned char)(m_len);
+							op[0] = (m_len);
 							op++;
 						}
-						op[0] = (unsigned char)(m_off << 2);
+						op[0] = (m_off << 2);
 						op++;
-						op[0] = (unsigned char)(m_off >> 6);
+						op[0] = (m_off >> 6);
 						op++;
 					}
 					cond_ipblock = ip_block < ip_block_end;
@@ -301,12 +313,12 @@ int main(int argc, char** argv)
 				unsigned char eq;
 				eq = op == outputBuffer;
 				if (eq && t <= 238) {
-					op[0] = (unsigned char)(17 + t);
+					op[0] = (17 + t);
 					op++;
 				} else if (t <= 3) {
-					op[0] |= (unsigned char)(t);
+					op[0] |= (t);
 				} else if (t <= 18) {
-					op[0] = (unsigned char)(t - 3);
+					op[0] = (t - 3);
 					op++;
 				} else
 				{
@@ -314,10 +326,10 @@ int main(int argc, char** argv)
 					op++;
 					unsigned int tt;
 					for (tt = t-18; tt > 255; tt-=255) {
-						((unsigned char *) op)[0] = 0;
+						op[0] = 0;
 						op++;
 					}
-					op[0] = (unsigned char)(tt);
+					op[0] = (tt);
 					op++;
 				}
 
